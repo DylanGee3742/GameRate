a841ab8423c48a1013b29ce7b1c9d5bb5195b812
Dylan Gee <dylangee3742@gmail.com>
2024-03-01 12:36:18 +0000

    title and review attriutes added, currently hard coded

--- src/functions/listFill.js
+++ src/functions/listFill.js
@@ -3,8 +3,8 @@ import { GameCard } from "../components/GameCard"
   // Make calls to API's in back-end to fill these in 
 export const listFill = (filter) => {
     let list = []
-    for (let i = 0; i < 30; i++) {
-        list.push({filter: filter, game: <GameCard filter={filter} />, id: i})
+    for (let i = 0; i < 8; i++) {
+        list.push({filter: filter, game: <GameCard filter={filter} />, id: i, review: "Heres some text", title: "Title"})
       }
     return list
 }
