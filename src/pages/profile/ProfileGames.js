f1095805c1071a823775200d8dce477743cc6118
Dylan Gee <dylangee3742@gmail.com>
2024-03-01 12:37:36 +0000

    profile games created and integrated

---
+++ src/pages/profile/ProfileGames.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GamesFiltered from '../../components/GamesFiltered'
+
+export default function ProfileGames() {
+  return (
+    <>
+    <div className='container'>
+        <GamesFiltered filter={"Played"} filterTitle={"Games"} />
+    </div>
+    </>
+  )
+}
