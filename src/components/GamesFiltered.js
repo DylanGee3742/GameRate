40a79a2def6568d5eee11fa62875acfda66a7eaf
Dylan Gee <dylangee3742@gmail.com>
2024-03-01 12:35:33 +0000

    dynamic values added

--- src/components/GamesFiltered.js
+++ src/components/GamesFiltered.js
@@ -17,12 +17,13 @@ export default function GamesFiltered(props) {
 
     return (
         <>
-            <h1>{title}</h1>
+            <h1 className='my-3'>{title}</h1>
+            <hr />
             <div clsssName="row d-flex overflow-auto">
                 {/* flex-wrap makes it start a new line, d-flex makes it inline */}
             <div className='col d-flex flex-wrap'>
             {gameObj.map((game, gameIndex) => (
-                          <div key={gameIndex} className="col-2 p-0 m-0">{game.game}</div>
+                          <div key={gameIndex} className="col-2 p-0 m-0">{game.game}{game.title}</div>
                         ))}
             </div>
             </div>
