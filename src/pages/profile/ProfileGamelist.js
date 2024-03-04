05779d22f26ee7895560f57ed5b50136ba231df2
Dylan Gee <dylangee3742@gmail.com>
2024-03-04 17:34:02 +0000

    GamesFiltered added to Games list page to show relevant games

--- src/pages/profile/ProfileGamelist.js
+++ src/pages/profile/ProfileGamelist.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import GamesFiltered from '../../components/GamesFiltered'
 
 export function ProfileGamelist() {
   return (
-    <div>ProfileGamelist</div>
+    <>
+    <div className='container'>
+    <GamesFiltered filter={"GameList"} filterTitle={"Game List"} />
+    </div>
+    </>
   )
 }
