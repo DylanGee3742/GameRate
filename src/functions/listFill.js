90ed8591d2e6554f339abbcfbf3cc28fc8a8e97b
Dylan Gee <dylangee3742@gmail.com>
2024-03-04 17:35:03 +0000

    default filter to trending if nothing else is selected

--- src/functions/listFill.js
+++ src/functions/listFill.js
@@ -2,9 +2,10 @@ import { GameCard } from "../components/GameCard"
 
   // Make calls to API's in back-end to fill these in 
 export const listFill = (filter) => {
+    const filterValue = filter || 'Trending'
     let list = []
     for (let i = 0; i < 8; i++) {
-        list.push({filter: filter, game: <GameCard filter={filter} />, id: i, review: "Heres some text", title: "Title"})
+        list.push({filterValue: filterValue, game: <GameCard filter={filter} />, id: i, review: "Heres some text", title: filterValue})
       }
     return list
 }
