743913b515d3e4439a2cfe82d1ec74d5ad00c88d
Dylan Gee <dylangee3742@gmail.com>
2024-03-23 12:02:37 +0000

    styling added, like and game list button added. refactored

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,54 +1,35 @@
-import React from 'react'
-import { useOutletContext, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
-import { faPlus as plus } from '@fortawesome/free-solid-svg-icons';
-import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
-import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons';
-import RateGameModal from '../components/RateGameModal.js';
+import { faStar as solidStar, faHeart as solidHeart, faGamepad as gamePad } from '@fortawesome/free-solid-svg-icons';
+import { faPlus as plus, faStar as regularStar, faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons';
+import RateGameModal from '../components/RateGameModal';
+import '../styles/GamePage.css';
 
 
 export function GamePage() {
-  const { id } = useParams()
-  const buttonStyle = {
-    background: 'none',
-    border: 'none',
-    padding: '0',
-    font: 'inherit',
-    cursor: 'pointer',
-    /* Add any additional custom styles here */
-  };
+  const [like, setLike] = useState(false);
+  const [onGameList, setOnGameList] = useState(false);
 
-  const videoStyle = {
-    width: '100%',
-    maxWidth: '800px',
-    maxHeight: '400px'
-  }
+  const { id } = useParams();
 
-  const coverStyle = {
-    height: '100%'
-  }
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [])
 
-  const friendsPic = {
-    maxHeight: '50px'
-  }
-
-  const friendsRating = {
-    maxHeight: '10px',
-    maxWidth: '10px'
-  }
 
   return (
     <>
       {/* Title, rating, add to list, rate */}
       {/* If screen size shrinks then move out rating, add to list, rate to below cover */}
+
       <div className='row m-2 align-items-center'>
         <div className='col-lg-7'>
           <h2>Game Title {id}</h2>
         </div>
         <div className='col text-center'>
           <p className="p-0 m-0"><strong>User Rating</strong></p>
-          <span className="d-inline-flex"><p className="p-0 m-0 ms-4"><strong>8.0/10</strong></p><FontAwesomeIcon icon={solidStar} className="m-1" style={{ color: 'gold' }} /></span>
+          <span className="d-inline-flex"><p className="p-0 m-0 ms-4"><strong>4.2/5</strong></p><FontAwesomeIcon icon={solidStar} className="goldStar m-1" /></span>
         </div>
         <div className='col text-center'>
           <p className="p-0 m-0"><strong>Rate</strong></p>
@@ -56,11 +37,17 @@ export function GamePage() {
         </div>
         <div className='col text-center'>
           <p className="p-0 m-0"><strong>Like</strong></p>
-          <span className="d-inline-flex"><button style={buttonStyle} ><FontAwesomeIcon icon={regularHeart} className="m-1" /></button></span>
+          <span className="d-inline-flex"><button className='buttonStyle' onClick={() => setLike(!like)} >
+            {like ? <FontAwesomeIcon icon={solidHeart} className=" redHeart m-1" /> : <FontAwesomeIcon icon={regularHeart} className="m-1" />}
+          </button>
+          </span>
         </div>
         <div className='col text-center'>
           <p className="p-0 m-0"><strong>Add to List</strong></p>
-          <span className="d-inline-flex"><button style={buttonStyle} ><FontAwesomeIcon icon={plus} className="m-1" /></button></span>
+          <span className="d-inline-flex"><button className='buttonStyle' onClick={() => setOnGameList(!onGameList)} >
+            {onGameList ? <FontAwesomeIcon icon={gamePad} className="m-1" /> : <FontAwesomeIcon icon={gamePad} className="unLiked m-1" />}
+          </button>
+          </span>
         </div>
 
       </div>
@@ -69,12 +56,10 @@ export function GamePage() {
       <div className='row m-2'>
         <div className='col d-flex align-items-center justify-content-center mt-4'>
           <img src="https://upload.wikimedia.org/wikipedia/en/f/f3/Call_of_Duty_%282003%29_cover.jpg"
-            className="img-fluid m-1 rounded"
-            style={coverStyle}
+            className="img-fluid m-1 rounded coverStyle"
             alt="Game Cover" />
           <img src="https://upload.wikimedia.org/wikipedia/en/f/f3/Call_of_Duty_%282003%29_cover.jpg"
-            className="img-fluid p-0 ms-2 rounded"
-            style={videoStyle}
+            className="img-fluid p-0 ms-2 rounded videoStyle"
             alt="Game Cover" />
         </div>
       </div>
@@ -102,53 +87,25 @@ export function GamePage() {
         <h6>Friends Activty</h6>
         {/* This will map out when have real data */}
         <ul className='list-group list-group-horizontal list-unstyled mt-3 '>
-
-          <li>
-            <div className="col me-4 text-center">
-              <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                className="img-fluid p-0 rounded-circle"
-                style={friendsPic}
-                alt="Game Cover" />
-              <p>Name</p>
-              <FontAwesomeIcon icon={solidStar} className="" style={{ color: 'gold' }} />
-              <FontAwesomeIcon icon={solidStar} className="" style={{ color: 'gold' }} />
-
-            </div>
-          </li>
-
-          <li>
-          <div className="col me-4 text-center">
-              <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                className="img-fluid p-0 rounded-circle"
-                style={friendsPic}
-                alt="Game Cover" />
-              <p>Name</p>
-              <FontAwesomeIcon icon={solidStar} className="" style={{ color: 'gold' }} />
-              <FontAwesomeIcon icon={solidStar} className="" style={{ color: 'gold' }} />
-              <FontAwesomeIcon icon={solidStar} className="" style={{ color: 'gold' }} />
-              <FontAwesomeIcon icon={solidStar} className="" style={{ color: 'gold' }} />
-              <FontAwesomeIcon icon={solidStar} className="" style={{ color: 'gold' }} />
-            </div>
-          </li>
-
-          <li>
-          <div className="col me-4 text-center">
-              <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                className="img-fluid p-0 rounded-circle"
-                style={friendsPic}
-                alt="Game Cover" />
-              <p>Name</p>
-              <FontAwesomeIcon icon={solidStar} className="" style={{ color: 'gold' }} />
-              <FontAwesomeIcon icon={solidStar} className="" style={{ color: 'gold' }} />
-              <FontAwesomeIcon icon={solidStar} className="" style={{ color: 'gold' }} />
-
-            </div>
-          </li>
+          {[...Array(3)].map((_, index) => (
+            <li key={index}>
+              <div className="col me-4 text-center">
+                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                  className="img-fluid p-0 rounded-circle friendsPic"
+                  alt="Game Cover" />
+                <p>Name</p>
+                {[...Array(index + 2)].map((_, i) => (
+                  <FontAwesomeIcon key={i} icon={solidStar} className="goldStar" />
+                ))}
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
 
       {/* Artwork, cast etc whatever can get */}
 
     </>
-  )
+  );
 }
+
