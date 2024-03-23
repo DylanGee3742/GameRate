f8aa2c485316cd7967f9753eb48154817a6e4858
Dylan Gee <dylangee3742@gmail.com>
2024-03-23 12:01:22 +0000

    now map links to game pages

--- src/components/GamesFiltered.js
+++ src/components/GamesFiltered.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { trendingGames, newReleases } from '../data/GameData';
 import { listFill } from './../functions/listFill';
 import { Games } from './../pages/GamesList';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export default function GamesFiltered(props) {
     const gameObj = listFill(props.filter)
@@ -16,7 +17,7 @@ export default function GamesFiltered(props) {
                 {/* flex-wrap makes it start a new line, d-flex makes it inline */}
             <div className='col d-flex flex-wrap'>
             {gameObj.map((game, gameIndex) => (
-                          <div key={gameIndex} className="col-2 p-0 m-0">{game.game}{game.title}</div>
+                          <NavLink key={gameIndex} to={`${game.id}`} gamePicture={game.game} gameTitle={game.title} className="col-md-3 mt-1">{game.game}{game.title}{game.id}</NavLink>
                         ))}
             </div>
             </div>
