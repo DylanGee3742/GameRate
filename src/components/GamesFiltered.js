b1c2a8bced00b67486122ee6f2c85580727ed792
Dylan Gee <dylangee3742@gmail.com>
2024-03-23 16:23:33 +0000

    undeeded console.log removed

--- src/components/GamesFiltered.js
+++ src/components/GamesFiltered.js
@@ -7,17 +7,16 @@ import { NavLink, useLocation } from 'react-router-dom';
 export default function GamesFiltered(props) {
     const gameObj = listFill(props.filter)
     const title = gameObj[0].title
-    console.log(gameObj)
 
     return (
         <>
             <h1 className='my-3'>{title}</h1>
             <hr />
-            <div clsssName="row d-flex overflow-auto">
+            <div className="row d-flex overflow-auto">
                 {/* flex-wrap makes it start a new line, d-flex makes it inline */}
             <div className='col d-flex flex-wrap'>
             {gameObj.map((game, gameIndex) => (
-                          <NavLink key={gameIndex} to={`${game.id}`} gamePicture={game.game} gameTitle={game.title} className="col-md-3 mt-1">{game.game}{game.title}{game.id}</NavLink>
+                          <NavLink key={gameIndex} to={`${game.id}`} gamepicture={game.game} gametitle={game.title} className="col-md-3 mt-1">{game.game}{game.title}{game.id}</NavLink>
                         ))}
             </div>
             </div>
