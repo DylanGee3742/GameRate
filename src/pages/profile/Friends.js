ff0d8721e3cef1cc6e0e242c8cee270c1c4ec40c
Dylan Gee <dylangee3742@gmail.com>
2024-03-25 11:55:11 +0000

    profile pic now acts a link to friends page. search for friend form added

--- src/pages/profile/Friends.js
+++ src/pages/profile/Friends.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Friends() {
 
@@ -9,8 +10,16 @@ export default function Friends() {
 
   return (
     <>
-    <div className='row border-bottom m-3'>
-      <h2 className='ms-3 mb-4'>Friends</h2>
+      <div className='row border-bottom m-3'>
+        <div className='col'>
+          <h2 className='ms-3 mb-4'>Friends</h2>
+        </div>
+        <div className='col d-flex justify-content-end'>
+          <p className="m-3 text-center">Find a Friend</p>
+          <form className="mt-2 form-inline">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+          </form>
+        </div>
       </div>
 
       <ul className='list-group list-group list-unstyled mt-3 ms-3 me-3'>
@@ -18,23 +27,23 @@ export default function Friends() {
           <li key={index}>
             <div className="row d-flex ms-5 mb-3">
               <div className='col-1 d-flex align-items-center justify-content-end'>
-                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                <NavLink to={`/friends/${index}`}> <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
                   className="img-fluid p-0 rounded-circle friendsPic"
                   style={friendProfilePic}
-                  alt="Game Cover" />
+                  alt="Game Cover" /></NavLink>
               </div>
               <div className='col'>
                 <div className='row d-flex '>
                   <div className='col-1 d-flex'>
-                  <p className='m-3 align-self-end'>Name</p>
+                    <p className='m-3 align-self-end'>Name</p>
                   </div>
                 </div>
                 <div className='row flex-row'>
                   <div className='col-2'>
-                  <p className='m-3'>Followers: 69</p>
+                    <p className='m-3'>Followers: 69</p>
                   </div>
                   <div className='col'>
-                  <p className='m-3 justify-content-start'>Followers: 75</p>
+                    <p className='m-3 justify-content-start'>Followers: 75</p>
                   </div>
                 </div>
               </div>
